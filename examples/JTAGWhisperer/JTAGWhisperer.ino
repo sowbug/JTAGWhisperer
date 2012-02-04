3ca12d05e9019afeb864139460123887512e2151
Mike Tsao <mike@sowbug.com>
2012-02-04 08:25:36 -0800

    Blinkenlights. Fix bps argument.

--- examples/JTAGWhisperer/JTAGWhisperer.ino
+++ examples/JTAGWhisperer/JTAGWhisperer.ino
@@ -14,7 +14,19 @@
 #include <JTAGWhisperer.h>
 #include <SerialComm.h>
 
+const int BLINK_PIN = 13;
+static bool is_pin_on;
+void blink() {
+  digitalWrite(BLINK_PIN, is_pin_on);
+  is_pin_on = !is_pin_on;
+}
+
 void setup() {
+  pinMode(BLINK_PIN, OUTPUT);
+  for (int i = 0; i < 10; ++i) {
+    blink();
+    delay(50);
+  }
 }
 
 // This is out of loop() so we get RAII without looping.
@@ -25,6 +37,7 @@ void do_it() {
   uint32_t instruction_count = 0;
 
   while (true) {
+    blink();
     uint8_t instruction = whisperer.read_next_instruction();
     if (!whisperer.handle_instruction(instruction)) {
       if (!whisperer.reached_xcomplete()) {
